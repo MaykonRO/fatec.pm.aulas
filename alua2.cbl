2546e8840c5d25af4f8b6b89e4e1b57c0a1c3386
lab56 <lab56@fatec.com>
2026-08-15 10:41:43 -0300

    feat: adiciona uma procedure

--- alua2.cbl
+++ alua2.cbl
@@ -12,5 +12,11 @@
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
+            DISPLAY "MEI LEGAL"
             STOP RUN.
+
+       CALC-PROCEDURE.
+            DISPLAY "Tem que somar 1 + 1"
+
+           STOP RUN.
        END PROGRAM aula2.
