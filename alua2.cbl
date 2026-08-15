4014c302d3ce02c2316c5d73c52611655597c549
lab56 <lab56@fatec.com>
2026-08-15 11:08:14 -0300

    feat: adiciona variavel para sobrenome

--- alua2.cbl
+++ alua2.cbl
@@ -10,15 +10,19 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01 NOME PIC X(30).
+       01 SOBRENOME PIC A(20).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
             DISPLAY "MEI LEGAL"
-
+            DISPLAY ""
+            DISPLAY "************************************"
             DISPLAY "Digite o seu nome".
             ACCEPT NOME.
+            DISPLAY "sobrenome".
+            ACCEPT SOBRENOME.
 
-            DISPLAY "Ola" NOME.
+            DISPLAY "Ola " NOME SOBRENOME.
 
             STOP RUN.
 
