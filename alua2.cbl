014514f4b1e496b3908328bf9b5bf9fd434551a6
lab56 <lab56@fatec.com>
2026-08-15 11:23:08 -0300

    feat: adiciona uma procedure, adiciona uma operação e execulta a operação de soma

--- alua2.cbl
+++ alua2.cbl
@@ -11,6 +11,9 @@
        WORKING-STORAGE SECTION.
        01 NOME PIC X(30).
        01 SOBRENOME PIC A(20).
+       01 NUMERO1 PIC 9(4).
+       01 NUMERO2 PIC 9(4).
+       01 RESULTADO PIC 9(5).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
@@ -24,10 +27,26 @@
 
             DISPLAY "Ola " NOME SOBRENOME.
 
+            PERFORM CALC-PROCEDURE.
+
+
             STOP RUN.
 
        CALC-PROCEDURE.
             DISPLAY "Tem que somar 1 + 1"
 
+            DISPLAY "digite um numero ate 9999".
+            ACCEPT NUMERO1.
+
+            DISPLAY "digite um numero ate 9999".
+            ACCEPT NUMERO2.
+
+            ADD NUMERO1 TO NUMERO2 GIVING RESULTADO.
+
+            DISPLAY "O resultado da operacao e' " RESULTADO.
+
            STOP RUN.
        END PROGRAM aula2.
+      * comando para remover o meu repositorio do git
+      * vai na raíz do repositorio  e digita isso no cmd
+      * git remote remove origin
