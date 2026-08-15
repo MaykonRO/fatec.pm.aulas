96b040759b7b231f05e3c0ecc9edffab5243f96d
lab56 <lab56@fatec.com>
2026-08-15 11:02:17 -0300

    feat: adiciona variavel de texto

--- alua2.cbl
+++ alua2.cbl
@@ -9,10 +9,17 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01 NOME PIC X(30).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
             DISPLAY "MEI LEGAL"
+
+            DISPLAY "Digite o seu nome".
+            ACCEPT NOME.
+
+            DISPLAY "Ola" NOME.
+
             STOP RUN.
 
        CALC-PROCEDURE.
