a1bf271b2196effda86b7a3e2cb61d9d37bd184b
lab56 <lab56@fatec.com>
2026-08-15 11:48:04 -0300

    feat: usa compute para fazer todas as operações

--- alua2.cbl
+++ alua2.cbl
@@ -28,13 +28,27 @@
 
             DISPLAY "Ola " NOME SOBRENOME.
 
-            PERFORM CALC-PROCEDURE.
+            PERFORM CALC-COMPUTE.
 
 
             STOP RUN.
 
-       CALC-PROCEDURE.
-            DISPLAY "Tem que somar 1 + 1"
+      *>  CALC-PROCEDURE.
+      *>       DISPLAY "Tem que somar 1 + 1"
+
+      *>       DISPLAY "digite um numero ate 9999".
+      *>       ACCEPT NUMERO1.
+
+      *>       DISPLAY "digite um numero ate 9999".
+      *>       ACCEPT NUMERO2.
+
+      *>       ADD NUMERO1 TO NUMERO2 GIVING RESULTADO.
+
+      *>       DISPLAY "O resultado da operacao e' " RESULTADO.
+
+      *>      STOP RUN.
+
+       CALC-COMPUTE.
 
             DISPLAY "digite um numero ate 9999".
             ACCEPT NUMERO1.
@@ -42,11 +56,13 @@
             DISPLAY "digite um numero ate 9999".
             ACCEPT NUMERO2.
 
-            ADD NUMERO1 TO NUMERO2 GIVING RESULTADO.
+            COMPUTE RESULTADO = NUMERO1 + NUMERO2.
 
             DISPLAY "O resultado da operacao e' " RESULTADO.
 
-           STOP RUN.
+            STOP RUN.
+
+
        END PROGRAM aula2.
       * comando para remover o meu repositorio do git
       * vai na raíz do repositorio  e digita isso no cmd
